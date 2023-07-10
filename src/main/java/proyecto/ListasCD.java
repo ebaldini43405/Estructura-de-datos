f1d5ec40c27e99c848a26a041696832203661593
AlonsoHernandez <hernandezalvarez.alonso@gmail.com>
2023-07-10 01:51:41 -0600

    Creacion de las listas dobles circulares, las cuales tendran como nodos cada una de las mesas con sus respectivos jugadores.

---
+++ src/main/java/proyecto/ListasCD.java
@@ -0,0 +1,38 @@
+package proyecto;
+
+public class ListasCD {
+    NodoCD primero;
+    NodoCD ultimo;
+
+    public ListasCD(){
+        primero = null;
+        ultimo = null;
+    }
+
+    public void insertar (Mesa m){
+        NodoCD nuevo = new NodoCD();
+        nuevo.mesa = m;
+        if(primero == null){
+            primero = nuevo;
+            primero.siguiente = primero;
+            nuevo.anterior = ultimo;
+            ultimo = nuevo;
+        }else{
+            ultimo.siguiente = nuevo;
+            nuevo.siguiente = primero;
+            nuevo.anterior = ultimo;
+            ultimo = nuevo;
+            primero.anterior = ultimo;
+        }
+    }
+    public void imprimirListaDC(){
+        NodoCD aux = primero;
+        int numeroMesa = 1;
+        do{
+            System.out.println("\n-----------------------------------------------\nMESA NUMERO="+numeroMesa);
+            aux.mesa.mostrarMesa();
+            aux = aux.siguiente;
+            numeroMesa++;
+        }while(aux != primero);
+    }
+}
--- src/main/java/proyecto/Main.java
+++ src/main/java/proyecto/Main.java
@@ -23,56 +23,41 @@ public class Main {
         System.out.println("\nTAMANO DE DECK DESARREGLADO = " + pila.tamanoPila());
         
         
-//        PilaDeck deck1  = new PilaDeck();
-//        PilaDeck deck2 = new PilaDeck();
-//        PilaDeck deck3 = new PilaDeck();
-//        PilaDeck deck4 = new PilaDeck();
-//        PilaDeck deck5 = new PilaDeck();
-//
-//        for(int i=0;i<7;i++)
-//        {
-//            deck1.repartir(deckDesordenado);
-//            deck2.repartir(deckDesordenado);
-//            deck3.repartir(deckDesordenado);
-//            deck4.repartir(deckDesordenado);
-//            deck5.repartir(deckDesordenado);
-//        }
-        
-//        Jugador jugador1 = new Jugador(deck1);
-//        Jugador jugador2 = new Jugador(deck2);
-//        Jugador jugador3 = new Jugador(deck3);
-//        Jugador jugador4 = new Jugador(deck4);
-//        Jugador jugador5 = new Jugador(deck5);
 
-        int numeroJugadores = Integer.parseInt(JOptionPane.showInputDialog("Cantidad de jugadores en la mesa"));
+
+
         Mesa mesa = new Mesa();
-        for(int i = 0; i<numeroJugadores; i++){
+        Mesa mesa1 = new Mesa();
+        for(int i = 0; i<3; i++){
 
             PilaDeck deck = new PilaDeck();
 
             for(int m=0;m<7;m++)
             {
                 deck.repartir(deckDesordenado);
-
             }
 
             Jugador jugador1 = new Jugador(deck);
 
             mesa.agregarJugadorMesa(jugador1);
         }
-        mesa.mostrarMesa();
-//        System.out.println("DECK JUGADOR 1:\n");
-//        deck1.imprimirDeck();
-//        System.out.println("\nDECK JUGADOR 2:\n");
-//        deck2.imprimirDeck();
-//        System.out.println("\nDECK JUGADOR 3:\n");
-//        deck3.imprimirDeck();
-//        System.out.println("\nDECK JUGADOR 4:\n");
-//        deck4.imprimirDeck();
-//        System.out.println("\nDECK JUGADOR 5:");
-//        deck5.imprimirDeck();
-        
-            
+        for(int i = 0; i<4; i++){
+
+            PilaDeck deck = new PilaDeck();
+
+            for(int m=0;m<7;m++)
+            {
+                deck.repartir(deckDesordenado);
+            }
+
+            Jugador jugador1 = new Jugador(deck);
+
+            mesa1.agregarJugadorMesa(jugador1);
+        }
+        ListasCD listaMesas = new ListasCD();
+        listaMesas.insertar(mesa);
+        listaMesas.insertar(mesa1);
+        listaMesas.imprimirListaDC();
         }
         
         
